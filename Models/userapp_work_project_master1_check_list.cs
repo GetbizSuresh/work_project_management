4b4bd9de8d2c2f51f27b6f5ab0a249af27123671
GetbizSuresh <suresh@getster.tech>
2022-03-18 15:42:11 +0530

    first commit

---
+++ Models/userapp_work_project_master1_check_list.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace getbiz_work_project_management.Models
+{
+    public class userapp_work_project_master1_check_list
+    {
+
+        public string check_list_s_no { set; get; }
+        public string id { set; get; }
+    
+        public string check_list_desc { set; get; }
+        public bool check_list_completed_verified { set; get; }
+    }
+}
